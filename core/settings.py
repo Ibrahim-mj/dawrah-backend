94259bbe1d2d63823d9ee98ccda8b56fed59eead
Ibrahim Abdulmojeed <ibraheemabdulmojeed1@gmail.com>
2023-12-03 11:33:58 +0100

    removed backend throttling

--- core/settings.py
+++ core/settings.py
@@ -164,14 +164,14 @@ REST_FRAMEWORK = {
     ],
     'SEARCH_PARAM': 'q',
     'SEARCH_FIELDS': ['username', 'email'],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/hour',
-        'user': '1000/hour'
-    },
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle'
+    # ],
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'anon': '100/hour',
+    #     'user': '1000/hour'
+    # },
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
